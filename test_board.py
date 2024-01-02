cc3302617a4389ba04ad073ab3d7c42e6570afdc
Septillion24 <noahgrosh@gmail.com>
2024-01-02 12:56:56 -0500

    redid tests bc i didnt like them

--- test_board.py
+++ test_board.py
@@ -1,38 +1,67 @@
 import pytest
 from Board import Board
 
-@pytest.fixture
-def board():
-    return Board()
-
-def test_initialization(board):
+def test_board_initialization():
+    board = Board()
     assert board.getBoardRows() == [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]]
 
-def test_setRowColumn(board):
-    board.setRowColumn(0, 0, 1)
+def test_set_row_column():
+    board = Board()
+    board.setRowColumn(0, 0, 1)  # Set 'x' at (0, 0)
     assert board.getRowColumn(0, 0) == 1
-    with pytest.raises(Exception):
-        board.setRowColumn(0, 0, 2)
 
-def test_checkWinningCondition(board):
+def test_set_row_column_invalid_value():
+    board = Board()
+    with pytest.raises(Exception):
+        board.setRowColumn(0, 0, 2)  # Invalid value
+
+def test_check_winning_condition():
+    board = Board()
     assert board.checkWinningCondition() == False
 
-def test_getWinningPlayer(board):
+def test_get_winning_player_no_winner():
+    board = Board()
     assert board.getWinningPlayer() == -1
 
-def test_checkRows(board):
-    board.setRowColumn(0, 0, 1)
-    board.setRowColumn(1, 0, 1)
-    board.setRowColumn(2, 0, 1)
+def test_check_rows_no_winner():
+    board = Board()
+    assert board.checkRows() == -1
+
+def test_check_columns_no_winner():
+    board = Board()
+    assert board.checkColumns() == -1
+
+def test_check_diagonals_no_winner():
+    board = Board()
+    assert board.checkDiagonals() == -1
+
+def test_check_values_for_win_no_winner():
+    board = Board()
+    assert board.checkValuesForWin([-1, 0, 1]) == -1
+
+# Add more tests to check the game logic
+def test_check_rows_winner():
+    board = Board()
+    board.setRowColumn(0, 0, 1)  # Set 'x' at (0, 0)
+    board.setRowColumn(1, 0, 1)  # Set 'x' at (1, 0)
+    board.setRowColumn(2, 0, 1)  # Set 'x' at (2, 0)
     assert board.checkRows() == 1
 
-def test_checkColumns(board):
-    board.setRowColumn(0, 0, 1)
-    board.setRowColumn(0, 1, 1)
-    board.setRowColumn(0, 2, 1)
-    assert board.checkColumns() == 1
+def test_check_columns_winner():
+    board = Board()
+    board.setRowColumn(0, 0, 0)  # Set 'o' at (0, 0)
+    board.setRowColumn(0, 1, 0)  # Set 'o' at (0, 1)
+    board.setRowColumn(0, 2, 0)  # Set 'o' at (0, 2)
+    assert board.checkColumns() == 0
 
-def test_checkValuesForWin(board):
-    assert board.checkValuesForWin([1, 1, 1]) == 1
-    assert board.checkValuesForWin([0, 0, 0]) == 0
-    assert board.checkValuesForWin([-1, -1, -1]) == -1
+def test_check_values_for_win_winner():
+    board = Board()
+    assert board.checkValuesForWin([1, 1, 1]) == 1  # 'x' wins
+    assert board.checkValuesForWin([0, 0, 0]) == 0  # 'o' wins
+    
+def test_check_diagonals_winner():
+    board = Board()
+    board.setRowColumn(0, 0, 1)  # Set 'x' at (0, 0)
+    board.setRowColumn(1, 1, 1)  # Set 'x' at (1, 1)
+    board.setRowColumn(2, 2, 1)  # Set 'x' at (2, 2)
+    assert board.checkDiagonals() == 1
