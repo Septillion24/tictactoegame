d24c3c258774cdbc79dfe1f95fb39c8897e26c85
Septillion24 <noahgrosh@gmail.com>
2024-02-04 13:13:01 -0500

    test program file

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+class Program
+{
+
+    static void parseUserInput(string input)
+    {
+        string pattern = @".*([0-2]).*([0-2]).*";
+        Regex regex = new Regex(pattern);
+        Match match = regex.Match(input);
+        if(!match.Success)
+        {
+            throw new ArgumentException();
+        }
+        else
+        {
+            int x = int.Parse(match.Groups[1].Value);
+            int y = int.Parse(match.Groups[2].Value);
+        }
+
+    }
+
+
+
+    static void Main()
+    {
+        Board board = new();
+        Console.WriteLine(board);
+    }
+}
